35c803707085d362f43a6f9a43b84581a8a449f4
Lando, Gabriel <gabriel.lando@hp.com>
2020-02-20 16:15:55 -0300

    Add project files.

---
+++ TorrentTray/SystemTray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using TorrentTray.Properties;
+
+namespace TorrentClient
+{
+    public class SystemTray : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public SystemTray()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
